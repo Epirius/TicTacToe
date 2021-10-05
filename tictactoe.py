99f43e8c616272997170f502df9a727d3bcf2e32
Epirius <68005825+Epirius@users.noreply.github.com>
2021-10-05 23:25:33 +0200

    .

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,12 @@ run = True
 
 def main():
 	while run:
+		# inputs:
 		size = int(input("Board size: "))
+		n_to_win = 0
+		while n_to_win <= 2 or n_to_win > size:
+			n_to_win = int(input(f"How many in a row to win? [3-{size}]: "))
+
 		board = Board(size)
 		print(board.update_board([1, 2]))
 		input()
